b2197936be55ea58cbe876f3a6e63e313b0ed515
iBarakaa <isaacbaraka1@gmail.com>
2024-01-07 17:56:48 +0300

    Routes, Assets, Components and Page initializations

---
+++ client/src/App.jsx
@@ -0,0 +1,63 @@
+import {  Outlet, Navigate, Route, Routes, useLocation  } from "react-router-dom";
+import {  Navbar, Footer  } from "./components";
+import {  About, AuthPage, FindJobs, JobDetail, ListerProfile, Listers, SeekerProfile, UploadJob  } from "./pages";
+
+// page movements
+function Layout () {
+  const user = false;
+  const location = useLocation();
+
+
+  return user ? (
+    <Outlet/>
+    ) : (
+      <Navigate to='user-auth' state={{ from: location  }} replace />
+  );
+  
+
+}
+
+function App() { 
+  const user = {};
+
+  return (
+    <main>
+      <Navbar />
+      <Routes>
+        {/* when authenticated */}
+          <Route element={<Layout/>}>
+            <Route path='/' 
+            element={<Navigate to='/find-jobs' replace={true}/>}
+            />
+
+          <Route path='/find-jobs' element={<FindJobs />} />
+
+          <Route path='/listers' element={<Listers />} />
+
+          <Route 
+            path={
+              user?.user?.accountType === "seeker"
+              ? "/seeker-profile"
+              : "/seeker-profile/:id"
+            }
+            element={<SeekerProfile />}
+            />
+
+            <Route path={"/lister-profile"} element={<ListerProfile />} />
+            <Route path={"/lister-profile/:id"} element={<ListerProfile />} />
+            <Route path={"/upload-job"} element={<UploadJob />} />
+            <Route path={"/job-detail/:id"} element={<JobDetail />} />
+          </Route>
+        {/* when unauthed */}
+          <Route path="/about-us" element={<About />} />
+          <Route path={"/user-auth"} element={<AuthPage />} />
+
+      </Routes>
+    {/* if user is not logged in show no footer */}
+      {user && <Footer />}
+    </main>
+
+  );
+}
+
+export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,19 @@
+import About from "./About";
+import AuthPage from "./AuthPage";
+import FindJobs from "./FindJobs";
+import JobDetail from "./JobDetail";
+import ListerProfile from "./ListerProfile";
+import Listers from "./Listers";
+import SeekerProfile from "./SeekerProfile";
+import UploadJob from "./UploadJob";
+
+export { 
+    About,
+    AuthPage,
+    FindJobs,
+    JobDetail,
+    ListerProfile,
+    Listers,
+    SeekerProfile,
+    UploadJob,
+ };
