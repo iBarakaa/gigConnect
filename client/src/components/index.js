a6aea0ed19f632fefde9cc23b009d730a2a7ac68
iBarakaa <isaacbaraka1@gmail.com>
2024-01-08 18:30:27 +0300

    Job Card and Job Postings frontend

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,6 +2,8 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CustomButton from "./CustomButton";
 import TextInput from "./TextInput"; 
-import SignUp from "./SignUp"
+import SignUp from "./SignUp";
+import ListBox from "./ListBox";
+import JobCard from "./JobCard";
 
-export {  Navbar, Footer, CustomButton, SignUp, TextInput  };
+export {  Navbar, Footer, CustomButton, SignUp, TextInput, ListBox, JobCard  };
