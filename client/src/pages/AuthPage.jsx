88b677e4745b377890ebe7ba0ac2f45c588aecd7
iBarakaa <isaacbaraka1@gmail.com>
2024-01-08 17:30:21 +0300

    User authentication modules

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,8 +1,26 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
-const AuthPage = () => {
-  return <div>AuthPage</div>;
-  
-}
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { Services } from "../assets";
+import { SignUp } from "../components";
 
-export default AuthPage;
+const Auth = () => {
+  const { user } = useSelector((state) => state.user);
+  const [open, setOpen] = useState(true);
+  const location = useLocation();
+
+  let from = location?.state?.from?.pathname || "/";
+
+  if (user.token) {
+    return window.location.replace(from);
+  }
+  return (
+    <div className='w-full '>
+      <img src={Services} alt='Services' className='object-contain ' />
+
+      <SignUp open={open} setOpen={setOpen} />
+    </div>
+  );
+};
+
+export default Auth;
